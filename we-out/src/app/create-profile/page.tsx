4087fdc62ad12a93fc458d6a42648dc7a4853b88
Adam Ma <adam.zym@gmail.com>
2023-11-05 12:48:58 -0500

    Added Template for Create Profile and Working on Start Swiping Page

--- we-out/src/app/create-profile/page.tsx
+++ we-out/src/app/create-profile/page.tsx
@@ -1,8 +1,87 @@
+"use client";
 
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { setDoc, doc, collection, getFirestore } from "firebase/firestore";
+import { auth } from "../firebase";
 
-export default function CreateProfile() {
+function CreateProfile() {
+  const [profilePic, setProfilePic] = useState<string>("");
+  const [daysAvailable, setDaysAvailable] = useState<string[]>([]);
+  const [timesAvailable, setTimesAvailable] = useState<{ [key: string]: { start: string, end: string } }>({});
+  const [experienceLevel, setExperienceLevel] = useState<string>("");
+
+  const handleCustomization = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: Save to Firestore
+  };
 
   return (
-    <p>hi</p>
+    <div style={{ display: "flex", justifyContent: "space-between", height: "92vh" }}>
+      <div className="Container">
+        <div className="pt-14 px-8">
+          <header className="Header font-sans font-bold">Profile Customization</header>
+          <Form onSubmit={handleCustomization}>
+            {/* Profile Picture Input */}
+            <Form.Group>
+              <Form.Label>Profile Picture</Form.Label>
+              <Form.Control type="file" onChange={e => setProfilePic(URL.createObjectURL(e.target.files[0]))} />
+            </Form.Group>
+
+            {/* Days Available */}
+            <Form.Group>
+              <Form.Label>Days Available</Form.Label>
+              {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
+                <Form.Check 
+                  type="checkbox"
+                  label={day}
+                  onChange={() => {
+                    if (daysAvailable.includes(day)) {
+                      setDaysAvailable(prev => prev.filter(d => d !== day));
+                    } else {
+                      setDaysAvailable(prev => [...prev, day]);
+                    }
+                  }}
+                />
+              ))}
+            </Form.Group>
+
+            {/* Times Available */}
+            <Form.Group>
+              <Form.Label>Times Available</Form.Label>
+              {daysAvailable.map(day => (
+                <div key={day}>
+                  <Form.Label>{day}</Form.Label>
+                  <Form.Control type="time" onChange={e => setTimesAvailable(prev => ({ ...prev, [day]: { ...prev[day], start: e.target.value } }))} />
+                  <Form.Control type="time" onChange={e => setTimesAvailable(prev => ({ ...prev, [day]: { ...prev[day], end: e.target.value } }))} />
+                </div>
+              ))}
+            </Form.Group>
+
+            {/* Experience Level */}
+            <Form.Group>
+              <Form.Label>Experience Level</Form.Label>
+              {['beginner', 'intermediate', 'advanced'].map(level => (
+                <Form.Check 
+                  type="radio"
+                  name="experienceLevel"
+                  label={level.charAt(0).toUpperCase() + level.slice(1)}
+                  onChange={() => setExperienceLevel(level)}
+                />
+              ))}
+            </Form.Group>
+
+            <Button
+              type="submit"
+              href="/dashboard"
+            >
+              Save
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default CreateProfile;
---
+++ we-out/src/app/dashboard/page.tsx
@@ -0,0 +1,36 @@
+import marino from '../../assets/marino.jpg'
+import Link from 'next/link'
+
+export default function Dashboard() {
+
+  return (
+
+    <div className="relative flex items-center justify-center h-screen bg-cover bg-no-repeat bg-center" 
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 0.9)), url(${marino.src})`}}>
+      <h1 className="absolute top-1/4 text-center text-6xl text-red-with-border">
+        Welcome to NUgym!
+      </h1>
+
+      <h1 className="absolute top-1/3 text-center text-3xl text-white">
+        Let's Get Started
+      </h1>
+      
+      <div className="z-10">
+
+        <div className="mt-8 flex flex-wrap justify-center gap-4">
+          <Link className="font-bold block w-full rounded border border-white bg-red-500 px-12 py-3 text-sm text-white hover:bg-red-800 hover:text-gray-500 focus:outline-none focus:ring active:text-opacity-75 sm:w-auto no-underline"
+            href="/start-swiping">
+            Start Swiping!
+          </Link>
+
+          <Link className="font-bold block w-full rounded border border-white bg-red-500 px-12 py-3 text-sm text-white hover:bg-red-800 hover:text-gray-500 focus:outline-none focus:ring sm:w-auto no-underline"
+            href="/learn-more">
+            Learn More
+          </Link>
+        </div>
+      </div>
+    </div>
+
+  );
+}
