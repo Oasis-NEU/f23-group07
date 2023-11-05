27ebae1d6e16903dfe3d68cab49109a0815a86f6
Adam Ma <adam.zym@gmail.com>
2023-11-05 13:19:10 -0500

    Modifications to Top Bar Header

--- we-out/src/app/common/header.tsx
+++ we-out/src/app/common/header.tsx
@@ -2,7 +2,7 @@ const Header = () => (
     <header className="bg-red-500 py-4 text-center">
       <nav>
         <a href="/" className="mx-4">Home</a>
-        <a href="/about" className="mx-4">About</a>
+        <a href="/learn-more" className="mx-4">Learn More</a>
         <a href="/contact" className="mx-4">Contact</a>
       </nav>
     </header>
