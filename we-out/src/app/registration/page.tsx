98250630af4b07582d8ea49515262c65c3a593a4
Win Tongtawee <WinTongtawee@gmail.com>
2023-11-05 14:11:49 -0500

    changed bootstrap version

--- we-out/src/app/registration/page.tsx
+++ we-out/src/app/registration/page.tsx
@@ -9,6 +9,7 @@ import login from "../../assets/login.jpg";
 import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
 
 function RegistrationScreen() {
+  const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmationPassword, setConfirmationPassword] = useState<string>("");
@@ -16,11 +17,11 @@ function RegistrationScreen() {
   const [confirmationPasswordShown, setConfirmationPasswordShown] =
     useState<boolean>(false);
   const [error, setError] = useState<string>("");
-  const [isClient, setIsClient] = useState<boolean>(true);
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
+
   const toggleConfirmationPassword = () => {
     setConfirmationPasswordShown(!confirmationPasswordShown);
   };
@@ -46,7 +47,9 @@ function RegistrationScreen() {
     const db = getFirestore();
     const id = user?.uid;
     const userRef = doc(collection(db, "users"), id);
-    setDoc(userRef, {});
+    setDoc(userRef, {
+      name: this.name
+    });
   };
 
   return (
@@ -62,12 +65,30 @@ function RegistrationScreen() {
           <header className="Header font-sans font-bold">Sign Up</header>
           <hr className="pt-0" />
           <Form className="Form" onSubmit={handleRegistration}>
+          <InputGroup className="mb-3" hasValidation>
+              <Form.Control
+                required
+                type="name"
+                placeholder="Full Name"
+                value={name}
+                className= "text-black"
+                isInvalid={name.trim().length === 0}
+                onChange={(e) => {
+                  const val = e.target.value;
+                  setName(val);
+                }}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name!
+              </Form.Control.Feedback>
+            </InputGroup>
             <InputGroup className="mb-3" hasValidation>
               <Form.Control
                 required
                 type="email"
                 placeholder="Email"
                 value={email}
+                className="text-black"
                 isInvalid={!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && !!email}
                 onChange={(e) => {
                   const val = e.target.value;
@@ -84,6 +105,7 @@ function RegistrationScreen() {
                 type={passwordShown ? "text" : "password"}
                 placeholder="Password"
                 value={password}
+                className="text-black"
                 onChange={(e) => {
                   const val = e.target.value;
                   setPassword(val);
@@ -105,6 +127,7 @@ function RegistrationScreen() {
                 type={confirmationPasswordShown ? "text" : "password"}
                 placeholder="Confirm Password"
                 value={confirmationPassword}
+                className="text-black"
                 isInvalid={
                   password !== confirmationPassword && !!confirmationPassword
                 }
@@ -134,9 +157,7 @@ function RegistrationScreen() {
             <Button
               variant="outline-secondary"
               type="submit"
-              className='SubmitButton'
-              href="/create-profile"
-            >
+              className='SubmitButton'>
               Sign up
             </Button>
             {error && <div className="text-danger mt-3">{error}</div>}
