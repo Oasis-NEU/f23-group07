9884ca86d6a7d12c53a605e01e09e28690faf9da
Adam Ma <adam.zym@gmail.com>
2023-11-12 14:05:02 -0500

    Data Fetching

--- we-out/src/app/dashboard/page.tsx
+++ we-out/src/app/dashboard/page.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardStack from "../swipe/cardstack";
 import { setDoc, getDocs, doc, collection, getFirestore } from "firebase/firestore";
 import { User } from "firebase/auth";
 
 function Dashboard() {
-  const getUsers = async () => {
-    const db = getFirestore();
-    const usersRef = collection(db, "users");
-    const usersSnapshot = await getDocs(usersRef);
-    const usersList = usersSnapshot.docs.map(doc => doc.data() as User);
-    return usersList;
-  }
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const db = getFirestore();
+      const usersRef = collection(db, "users");
+      const usersSnapshot = await getDocs(usersRef);
+      const usersList = usersSnapshot.docs.map(doc => doc.data()); // Assuming User type is handled
+      setUsers(usersList);
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <div>
-      <CardStack users={getUsers()} />
+      <CardStack users={users} />
     </div>
   );
 }
