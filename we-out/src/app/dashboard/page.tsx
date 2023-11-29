d75bf9c03c7f58e0dc60249a1aa718c18f1dd343
Win Tongtawee <WinTongtawee@gmail.com>
2023-11-29 17:26:21 -0500

    set up basic cards

--- we-out/src/app/dashboard/page.tsx
+++ we-out/src/app/dashboard/page.tsx
@@ -1,24 +1,40 @@
 'use client';
-
-import { useState, useEffect } from "react";
-import CardStack from "../swipe/cardstack";
-import 
+import React, { useState, useEffect } from 'react';
 
 function Dashboard() {
-  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
+  const data = [
+    {
+      "firstName": "John",
+      "lastName": "Doe"
+    },
+    {
+      "firstName": "Jane",
+      "lastName": "Doe"
+    },
+    {
+      "firstName": "Bob",
+      "lastName": "Smith"
+    }
+  ]
 
-      setUsers(usersList);
-    };
 
-    fetchUsers();
-  }, []);
 
   return (
-    <div>
-      <CardStack users={users} />
+    <div style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', top: 0, right: 0 }}>
+        <button onClick={() => alert('Edit profile')}>Edit Profile</button>
+        <button onClick={() => alert('Log out')}>Log Out</button>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {data.map((item, index) => (
+          <div key={index} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
+            <p>{item.firstName}</p>
+            <button onClick={() => alert('Liked')}>Like</button>
+            <button onClick={() => alert('Rejected')}>Reject</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
