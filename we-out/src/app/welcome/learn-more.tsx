613fcf335ed13c0cd0c68998e6034d5f879e2fcd
Win Tongtawee <WinTongtawee@gmail.com>
2023-12-01 03:44:30 -0500

    changed css

--- we-out/src/app/welcome/learn-more.tsx
+++ we-out/src/app/welcome/learn-more.tsx
@@ -11,34 +11,20 @@ export default function LearnMore() {
         </h1>
         <div className="mt-8 flex flex-wrap justify-center gap-4">
         </div>
-      <div className="rounded-box-left">
         <div className="text-box">
-          <p className="font-style">
+          <p className="text-box-font-style">
           FitBuddyNU is a dynamic web application designed exclusively for students at Northeastern University, 
           with the primary goal of fostering a sense of community and helping users achieve their fitness objectives. 
           This user-friendly platform allows students to connect with like-minded peers who share similar fitness goals
           and interests, enabling them to team up for productive and enjoyable gym sessions. 
-          </p>
-        </div>
-      </div>
-      <div className="rounded-box-right">
-        <div className="text-box">
-          <p className="font-style">
-            Key features of FitBuddyNU include: <br /><br />
+          <br /><br />
+          
+          Key features of FitBuddyNU include: <br /><br />
           • Students can create personalized profiles by specifying their fitness goals, preferred workout routines, and availability.<br /><br />
           • Sophisticated algorithm to suggest potential workout partners based on common fitness goals, availability, and location within the university campus.<br /><br />
           • Communicate with potential gym buddies through a built-in messaging system, making it easy to plan workouts and coordinate schedules.<br /><br />
-
-
-
-
-          
           </p>
-        </div>
       </div>
-
-
-
     </div>
 
   );
