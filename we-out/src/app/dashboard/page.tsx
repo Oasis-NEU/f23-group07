34de66feb3ffc1f060a9b9b99a264261310f7074
Adam Ma <adam.zym@gmail.com>
2023-11-30 21:55:08 -0500

    User Cards on Dashboard with Sample Data

--- we-out/src/app/dashboard/page.tsx
+++ we-out/src/app/dashboard/page.tsx
@@ -1,24 +1,102 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import marino from '../../assets/marino.jpg'
+
 
 function Dashboard() {
 
   const data = [
     {
       "firstName": "John",
-      "lastName": "Doe"
+      "lastName": "Doe",
+      "profilePicture": marino.src,
+
+      "mondayAvailable": true,
+      "tuesdayAvailable": false,
+      "wednesdayAvailable": true,
+      "thursdayAvailable": false,
+      "fridayAvailable": true,
+      "saturdayAvailable": false,
+      "sundayAvailable": true,
+
+      "experienceLevel": "Beginner"
     },
     {
       "firstName": "Jane",
-      "lastName": "Doe"
+      "lastName": "Doe",
+      "profilePicture": marino.src,
+
+      "mondayAvailable": true,
+      "tuesdayAvailable": false,
+      "wednesdayAvailable": true,
+      "thursdayAvailable": false,
+      "fridayAvailable": true,
+      "saturdayAvailable": false,
+      "sundayAvailable": true,
+
+      "experienceLevel": "Intermediate"
     },
     {
       "firstName": "Bob",
-      "lastName": "Smith"
+      "lastName": "Smith",
+      "profilePicture": "",
+
+      "mondayAvailable": true,
+      "tuesdayAvailable": false,
+      "wednesdayAvailable": true,
+      "thursdayAvailable": false,
+      "fridayAvailable": true,
+      "saturdayAvailable": false,
+      "sundayAvailable": true,
+
+      "experienceLevel": "Advanced"
     }
   ]
 
+  //TODO: Fetch user data from the database
 
+
+
+  function getDaysAvailable(
+    mondayAvailable: boolean,
+    tuesdayAvailable: boolean,
+    wednesdayAvailable: boolean,
+    thursdayAvailable: boolean,
+    fridayAvailable: boolean,
+    saturdayAvailable: boolean,
+    sundayAvailable: boolean
+  ) {
+    const daysAvailable = [];
+
+    if (mondayAvailable) {
+      daysAvailable.push("Monday");
+    }
+
+    if (tuesdayAvailable) {
+      daysAvailable.push("Tuesday");
+    }
+
+    if (wednesdayAvailable) {
+      daysAvailable.push("Wednesday");
+    }
+
+    if (thursdayAvailable) {
+      daysAvailable.push("Thursday");
+    }
+
+    if (fridayAvailable) {
+      daysAvailable.push("Friday");
+    }
+
+    if (saturdayAvailable) {
+      daysAvailable.push("Saturday");
+    }
+
+    if (sundayAvailable) {
+      daysAvailable.push("Sunday");
+    }
+
+    return daysAvailable;
+  }
 
   return (
     <div style={{ position: 'relative' }}>
@@ -29,7 +107,28 @@ function Dashboard() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {data.map((item, index) => (
           <div key={index} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
-            <p>{item.firstName}</p>
+            
+            {/* Profile Picture */}
+            <img src={item.profilePicture} alt='Profile Picture' width="500"/>
+
+            {/* First and Last Name */}
+            <p>{item.firstName + " " + item.lastName}</p>
+
+            {/* Days Available */}
+            <p>{"Days Available: " + getDaysAvailable(
+              item.mondayAvailable,
+              item.tuesdayAvailable,
+              item.wednesdayAvailable,
+              item.thursdayAvailable,
+              item.fridayAvailable,
+              item.saturdayAvailable,
+              item.sundayAvailable
+            ).join(", ")}</p>
+
+            {/* Experience Level */}
+            <p>{"Experience Level: " + item.experienceLevel}</p>
+
+            {/* Like and Reject Buttons */}
             <button onClick={() => alert('Liked')}>Like</button>
             <button onClick={() => alert('Rejected')}>Reject</button>
           </div>
