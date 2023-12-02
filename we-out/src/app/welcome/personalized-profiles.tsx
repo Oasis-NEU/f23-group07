822c747793ffa9793178bd21baa22271d1e0eacc
Adam Ma <adam.zym@gmail.com>
2023-12-01 20:57:18 -0500

    More Changes to Front Page

--- we-out/src/app/welcome/personalized-profiles.tsx
+++ we-out/src/app/welcome/personalized-profiles.tsx
@@ -29,7 +29,7 @@ export default function PersonalizedProfiles() {
                 href="#"
                 className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
               >
-                Get Started Today
+                Awesome!
               </a>
             </div>
           </div>
