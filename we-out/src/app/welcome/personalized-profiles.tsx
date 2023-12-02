a1300fa5bba0d4f607755ac91603e7c7e5daf988
Win Tongtawee <WinTongtawee@gmail.com>
2023-12-01 20:47:25 -0500

    removed button

--- we-out/src/app/welcome/personalized-profiles.tsx
+++ we-out/src/app/welcome/personalized-profiles.tsx
@@ -24,13 +24,6 @@ export default function PersonalizedProfiles() {
                 are most compatible with the aforementioned attributes. At NUgym, we believe that
                 working out with a partner is more fun and motivating than working out alone.
               </p>
-
-              <a
-                href="#"
-                className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-              >
-                Get Started Today
-              </a>
             </div>
           </div>
         </div>
